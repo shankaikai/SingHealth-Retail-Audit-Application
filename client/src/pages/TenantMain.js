4f4488250e8daecc4a247143c9d0edb7acffa332
sazzyboy <43925277+sazzyboy@users.noreply.github.com>
2021-04-08 19:36:52 +0800

    copied files for tenant pages over

--- client/src/pages/TenantMain.js
+++ client/src/pages/TenantMain.js
@@ -1,6 +1,11 @@
 import React, { useContext } from "react";
 import { LoginContext } from "../context/LoginContext";
 import { Redirect, Route, BrowserRouter as Router } from "react-router-dom";
+import TenantAuditIssuePage from "./tenant/TenantAuditIssuePage";
+import TenantNewMessagePage from "./tenant/TenantNewMessage";
+import TenantOutstandingPage from "./tenant/TenantOutstandingPage";
+import TenantProfilePage from "./tenant/TenantProfilePage";
+
 
 export default function TenantMain(props) {
   const { context } = useContext(LoginContext);
@@ -10,8 +15,22 @@ export default function TenantMain(props) {
       {context.id === null ? (
         <Redirect to="/login" />
       ) : (
-        <h1>Tenant Main not done</h1>
+        <Router>
+          <Route exact path="/outstanding">
+            <TenantOutstandingPage/>
+          </Route>
+          <Route exact path="/account">
+            <TenantProfilePage/>
+          </Route>
+          <Route exact path="/issue/:id">
+            <TenantAuditIssuePage/>
+          </Route>
+          <Route exact path="/newmessage">
+            <TenantNewMessagePage/>
+          </Route>
+        </Router>
       )}
     </div>
   );
 }
+
---
+++ client/src/pages/tenant/TenantAuditIssuePage.js
@@ -0,0 +1,107 @@
+import { makeStyles, List } from "@material-ui/core";
+import Header from "../../components/common/Header";
+import { useLocation, useParams } from "react-router-dom";
+import IssueItem from "../../components/tenantView/IssueItem";
+import { LoginContext } from "../../context/LoginContext";
+import { useContext } from "react"
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import MessageItem from "../../components/issueHandling/MessageItem"
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  form: {
+    marginTop: "63px",
+    width: "80%",
+    paddingTop: "20px",
+  },
+  submit: {
+    width: "80%",
+    marginBottom: "30px",
+  },
+  field: {
+    marginBottom: "30px",
+  },
+  messageContainer: {
+    overflowY: "auto",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    alignItems: "center",
+    marginTop: "63px",
+    height: "calc(100vh - 63px)",
+    // paddingTop: "20px",
+    // paddingBottom: "20px",
+  },
+});
+
+const TenantAuditIssuePage = () => {
+  const classes = useStyles();
+  const { context } = useContext(LoginContext);
+  const { id } = useParams();
+
+  const [issueData, setIssueData] = useState(0);
+  const [messageData, setMessageData] = useState(0);
+  const [userNames, setUserNames] = useState(0);
+  const [issueDataTranslated, setIssueDataTranslated] = useState(0);
+
+  useEffect(() => {
+    Axios.get(`http://localhost:3001/tenant/issue/${id}`).then((response) => {
+
+      setIssueData(response.data.issue);
+      setMessageData(response.data.messages.reverse()); 
+      console.log(response.data.issue);
+      // console.log(response.data.issue[0].description);
+      
+      let users = {
+        "tenant":response.data.tenant,
+        "staff":response.data.staff,
+      };
+
+      let issueMessageTranslate = {
+        "isStaff":1,
+        "dateSent": response.data.issue[0].date,
+        "body": response.data.issue[0].description,
+        "photoUrl":response.data.issue[0].imageUrl,
+      }
+      
+      setUserNames(users);
+      setIssueDataTranslated(issueMessageTranslate);
+    });
+  }, []);
+
+  // Submit handler
+  const handleSubmit = () => {
+    //TODO: Axios post req
+    console.log("Submit");
+  };
+
+  return (
+    <div className={classes.root}>
+      <Header back title={context.name} noDivider />
+      <div className={classes.messageContainer}>
+        <List>
+          {messageData ?
+            messageData.map((data) => (
+              <MessageItem
+                key={data.id}
+                data={data}
+                users={userNames}
+              />
+            ))
+            : null}
+            {issueDataTranslated?
+            <MessageItem data={issueDataTranslated} users={userNames}/>
+            :null}
+        </List>
+      </div>
+    </div>
+  );
+};
+
+export default TenantAuditIssuePage;
